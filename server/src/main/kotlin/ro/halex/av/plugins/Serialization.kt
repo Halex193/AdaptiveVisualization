e82fd1318ff7d375a915b4419662b6004534b17d
Horațiu Udrea <udrea.horatiu@gmail.com>
2021-05-16 22:57:40 +0300

    Ignore module server-side

--- server/src/main/kotlin/ro/halex/av/plugins/Serialization.kt
+++ server/src/main/kotlin/ro/halex/av/plugins/Serialization.kt
@@ -6,9 +6,10 @@ import io.ktor.application.*
 import io.ktor.response.*
 import io.ktor.request.*
 import io.ktor.routing.*
+import kotlinx.serialization.json.Json
 
 fun Application.configureSerialization() {
     install(ContentNegotiation) {
-        json()
+        json(Json { ignoreUnknownKeys = true })
     }
 }
